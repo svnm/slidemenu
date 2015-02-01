ed67d9184a4d96959ad4e4217bc2706cb80f3aa8
Steven Martin <stevenisekimartin@gmail.com>
2015-02-01 12:34:47 +1100

    update to gulpfile and include reference to snap.svg

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,8 @@
 var gulp = require('gulp'),
-  less = require('gulp-less'),
-  concatCss = require('gulp-concat-css'),
   minify = require('gulp-minify-css'),
   rename = require('gulp-rename');
 
-gulp.task('concatCss', function (cb) {
-  var stream = gulp.src('css/**/*.css')
-    .pipe(concatCss("slidemenu.css"))
-    .pipe(gulp.dest('.'));
-  return stream;
-});
-
-gulp.task('minify', ['concatCss'], function (cb) {
+gulp.task('minify', function (cb) {
   var stream = gulp.src('slidemenu.css')
     .pipe(minify({keepBreaks: true}))
     .pipe(rename({
