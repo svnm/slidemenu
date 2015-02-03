1e31b930ae572b5c9805c3d5c141049d702e5131
Steven Martin <stevenisekimartin@gmail.com>
2015-02-03 16:17:25 +1100

    update bower, html structure and slide css module

--- slidemenu.js
+++ slidemenu.js
@@ -1,17 +1,4 @@
-/* classie, or lunar??? */
-
-/*!
- * classie - class helper functions
- * from bonzo https://github.com/ded/bonzo
- * 
- * classie.has( elem, 'my-class' ) -> true/false
- * classie.add( elem, 'my-new-class' )
- * classie.remove( elem, 'my-unwanted-class' )
- * classie.toggle( elem, 'my-class' )
- */
-
-/*jshint browser: true, strict: true, undef: true */
-/*global define: false */
+/* classie */
 
 ( function( window ) {
 
@@ -24,7 +11,6 @@ function classReg( className ) {
 }
 
 // classList support for class management
-// altho to be fair, the api sucks because it won't accept multiple classes at once
 var hasClass, addClass, removeClass;
 
 if ( 'classList' in document.documentElement ) {
@@ -70,20 +56,13 @@ var classie = {
   toggle: toggleClass
 };
 
-// transport
-if ( typeof define === 'function' && define.amd ) {
-  // AMD
-  define( classie );
-} else {
-  // browser global
-  window.classie = classie;
-}
+// browser global
+window.classie = classie;
 
 })( window );
 
 
-
-/* svg menu */
+/* svg slide menu */
 
 function SVGMenu( el, options ) {
   this.el = el;
@@ -91,7 +70,7 @@ function SVGMenu( el, options ) {
 }
 
 SVGMenu.prototype.init = function() {
-  this.trigger = this.el.querySelector( 'button.menu__handle' );
+  this.trigger = this.el.querySelector( 'button.slide-menu-handle' );
   this.shapeEl = this.el.querySelector( 'div.morph-shape' );
 
   var s = Snap( this.shapeEl.querySelector( 'svg' ) );
@@ -107,17 +86,16 @@ SVGMenu.prototype.init = function() {
   this.trigger.addEventListener( 'click', this.toggle.bind(this) );
 };
 
-
 SVGMenu.prototype.toggle = function() {
   var self = this;
 
   if( this.isOpen ) {
     classie.remove( self.el, 'menu--anim' );
-    setTimeout( function() { classie.remove( self.el, 'menu--open' ); }, 250 );
+    setTimeout( function() { classie.remove( self.el, 'slide-menu-open' ); }, 250 );
   }
   else {
     classie.add( self.el, 'menu--anim' );
-    setTimeout( function() { classie.add( self.el, 'menu--open' );  }, 250 );
+    setTimeout( function() { classie.add( self.el, 'slide-menu-open' );  }, 250 );
   }
   self.pathEl.stop().animate( { 'path' : self.isOpen ? self.paths.close : this.paths.open }, 350, mina.easeout, function() {
     self.pathEl.stop().animate( { 'path' : self.paths.reset }, 800, mina.elastic );
@@ -126,4 +104,5 @@ SVGMenu.prototype.toggle = function() {
   self.isOpen = !self.isOpen;
 };
 
-new SVGMenu( document.getElementById( 'menu' ) );
+new SVGMenu( document.getElementById( 'slide-menu' ) );
+
